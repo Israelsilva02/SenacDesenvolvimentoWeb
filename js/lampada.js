543d71441efc9a10a6974e237feeef1a5402991f
ISRAEL BERNARDO DA SILVA <israel.bsilva8@senacsp.edu.br>
2023-08-22 22:29:45 -0300

    Atualizando

--- js/lampada.js
+++ js/lampada.js
@@ -1,1 +1,19 @@
-console.log('teste');
+
+document.getElementById("lampada").addEventListener("lampadaacesa", lampadaEmCima) ;
+document.getElementById("lampada").addEventListener("lampadaapagada",lampaEmAbaixo);
+
+function lampadaEmCima(){
+    let elemento = document.getElementById("lampada");
+
+    elemento.style.cursor = "hand";
+    elemento.style.backgroundColor = "cyan";
+    elemento.innerHTML = "lampadaacesa";
+}
+
+function lampaEmAbaixo(){
+
+    let elemento = document.getElementById("lampada");
+
+    elemento.style.backgroundColor = "purple";
+    elemento.innerHTML = "lampadaapagada";
+}
